b8c489450d2e3cd55262784aecae54afe3ef1bf6
Luiz Picolo <luizpicolo@gmail.com>
2014-05-19 19:34:14 -0400

    Commit inicial

---
+++ src/br/edu/ifms/agenda/model/Categoria.java
@@ -0,0 +1,58 @@
+package br.edu.ifms.agenda.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String titulo;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@OneToMany
+	private List<Tarefa> tarefas;
+	
+	
+	/* Getters e Setters */
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Tarefa> getTarefas() {
+		return tarefas;
+	}
+
+	public void setTarefas(List<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+	}
+}
---
+++ src/br/edu/ifms/agenda/model/Usuario.java
@@ -0,0 +1,121 @@
+package br.edu.ifms.agenda.model;
+
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+public abstract class Usuario {
+	@Id @GeneratedValue(strategy=GenerationType.TABLE)
+	private long id;
+	private String nome;
+	private String email;
+	private String avatar;
+	private String descricao;
+	private String usuario;
+	private String senha;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar dataCadastro;
+	
+	@OneToMany(mappedBy="usuario")
+	private List<Categoria> categorias;
+	
+	@ManyToOne
+	private Instituicao instituicao;
+	
+	
+	/* Getters e Setters */
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Calendar getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Calendar dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	public Instituicao getInstituicao() {
+		return instituicao;
+	}
+
+	public void setInstituicao(Instituicao instituicao) {
+		this.instituicao = instituicao;
+	}
+}
