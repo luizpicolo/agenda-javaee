223193f3dd83d38550cafec4d8a1e9437ef6dad0
Luiz Picolo <luizpicolo@gmail.com>
2014-05-19 20:00:41 -0400

    Teste de persistencia

--- src/br/edu/ifms/agenda/model/Teste.java
+++ src/br/edu/ifms/agenda/model/Teste.java
@@ -1,5 +1,8 @@
 package br.edu.ifms.agenda.model;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import javax.persistence.EntityManager;
 
 import br.edu.ifms.agenda.utils.JPAUtil;
@@ -11,12 +14,51 @@ public class Teste {
 		EntityManager em = new JPAUtil().getEntityManager();
 
 		/* Cria Instituições */
-		Instituicao ifms = new Instituicao();
+		Instituicao instituicao = new Instituicao();
 
-		ifms.setNome("IFMS");
+		instituicao.setNome("IFMS");
 
 		em.getTransaction().begin();
-		em.persist(ifms);
+		em.persist(instituicao);
+		em.getTransaction().commit();
+
+		// Cria professor
+		Professor professor = new Professor();
+		professor.setNome("Evandro");
+		professor.setInstituicao(instituicao);
+
+		em.getTransaction().begin();
+		em.persist(professor);
+		em.getTransaction().commit();
+
+		// Cria aluno
+		Aluno aluno = new Aluno();
+		aluno.setNome("PacoBoy");
+		aluno.setInstituicao(instituicao);
+
+		em.getTransaction().begin();
+		em.persist(aluno);
+		em.getTransaction().commit();
+		
+		// Cria Categoria
+		Categoria categoria = new Categoria();
+		categoria.setTitulo("Gerencia de Projetos");
+		
+		em.getTransaction().begin();
+		em.persist(categoria);
+		em.getTransaction().commit();
+
+		// Cria tarefa
+		Tarefa tarefa = new Tarefa();
+		tarefa.setTitulo("Trabalho do Dinho");
+		tarefa.setDescricao("Copa do mundo");
+		tarefa.setDataInicio(new GregorianCalendar(2014, 11, 12, 10, 00, 00));
+		tarefa.setDataTermino(new GregorianCalendar(2014, 11, 12, 12, 00, 00));
+		tarefa.setCategoria(categoria);
+		tarefa.setProfessor(professor);
+		
+		em.getTransaction().begin();
+		em.persist(tarefa);
 		em.getTransaction().commit();
 	}
 
