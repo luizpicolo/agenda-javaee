ea6281bc42aba8f9ec4e705caedb2b9fe30b6c8f
Luiz Picolo <luizpicolo@gmail.com>
2014-05-20 20:07:07 -0400

    Iniciada implementação das classes DAO

--- src/br/edu/ifms/agenda/model/Teste.java
+++ src/br/edu/ifms/agenda/model/Teste.java
@@ -4,14 +4,19 @@ import java.util.GregorianCalendar;
 
 import javax.persistence.EntityManager;
 
+import br.edu.ifms.Dao.DaoInstituicao;
 import br.edu.ifms.agenda.utils.JPAUtil;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		/* Criação da EntitiManager em */
-		EntityManager em = new JPAUtil().getEntityManager();
-
+		
+		
+		
+		
+//		/* Criação da EntitiManager em */
+//		EntityManager em = new JPAUtil().getEntityManager();
+//
 		/* Cria Instituições */
 		Instituicao instituicao = new Instituicao();
 
@@ -19,49 +24,56 @@ public class Teste {
 		instituicao.setCnpj("0000000000000");
 		instituicao.setEmail("ifms@ifms.edu.br");
 		
-
-		em.getTransaction().begin();
-		em.persist(instituicao);
-		em.getTransaction().commit();
-
-		// Cria professor
-		Professor professor = new Professor();
-		professor.setNome("Evandro");
-		professor.setInstituicao(instituicao);
-
-		em.getTransaction().begin();
-		em.persist(professor);
-		em.getTransaction().commit();
-
-		// Cria aluno
-		Aluno aluno = new Aluno();
-		aluno.setNome("PacoBoy");
-		aluno.setInstituicao(instituicao);
-
-		em.getTransaction().begin();
-		em.persist(aluno);
-		em.getTransaction().commit();
+		DaoInstituicao dao = new DaoInstituicao();
+		Instituicao inst = dao.salvar(instituicao);
+		System.out.println(inst.getId());
 		
-		// Cria Categoria
-		Categoria categoria = new Categoria();
-		categoria.setTitulo("Gerencia de Projetos");
 		
-		em.getTransaction().begin();
-		em.persist(categoria);
-		em.getTransaction().commit();
+//
+//		em.getTransaction().begin();
+//		em.persist(instituicao);
+//		em.getTransaction().commit();
+//		
+//		System.out.println(instituicao.getId());
 
-		// Cria tarefa
-		Tarefa tarefa = new Tarefa();
-		tarefa.setTitulo("Trabalho do Dinho");
-		tarefa.setDescricao("Copa do mundo");
-		tarefa.setDataInicio(new GregorianCalendar(2014, 11, 12, 10, 00, 00));
-		tarefa.setDataTermino(new GregorianCalendar(2014, 11, 12, 12, 00, 00));
-		tarefa.setCategoria(categoria);
-		tarefa.setProfessor(professor);
-		
-		em.getTransaction().begin();
-		em.persist(tarefa);
-		em.getTransaction().commit();
+//		// Cria professor
+//		Professor professor = new Professor();
+//		professor.setNome("Evandro");
+//		professor.setInstituicao(instituicao);
+//
+//		em.getTransaction().begin();
+//		em.persist(professor);
+//		em.getTransaction().commit();
+//
+//		// Cria aluno
+//		Aluno aluno = new Aluno();
+//		aluno.setNome("PacoBoy");
+//		aluno.setInstituicao(instituicao);
+//
+//		em.getTransaction().begin();
+//		em.persist(aluno);
+//		em.getTransaction().commit();
+//		
+//		// Cria Categoria
+//		Categoria categoria = new Categoria();
+//		categoria.setTitulo("Gerencia de Projetos");
+//		
+//		em.getTransaction().begin();
+//		em.persist(categoria);
+//		em.getTransaction().commit();
+//
+//		// Cria tarefa
+//		Tarefa tarefa = new Tarefa();
+//		tarefa.setTitulo("Trabalho do Dinho");
+//		tarefa.setDescricao("Copa do mundo");
+//		tarefa.setDataInicio(new GregorianCalendar(2014, 11, 12, 10, 00, 00));
+//		tarefa.setDataTermino(new GregorianCalendar(2014, 11, 12, 12, 00, 00));
+//		tarefa.setCategoria(categoria);
+//		tarefa.setProfessor(professor);
+//		
+//		em.getTransaction().begin();
+//		em.persist(tarefa);
+//		em.getTransaction().commit();
 	}
 
 }
